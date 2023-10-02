e8f8e1d5ea04f61a2fa176f84640fed3f06b2316
Tyler Hudson <tylerhudson96@gmail.com>
2023-10-02 18:46:56 -0400

    Moved buildTree into class Tree as a method feeding in the root node

--- Tree.js
+++ Tree.js
@@ -3,23 +3,23 @@ import Node from "./Node.js";
 //Testing Arr
 const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-function buildTree(arr, start = 0, end = arr.length - 1) {
-  if (start > end) {
-    return null;
-  }
-
-  let mid = Math.floor(parseInt((start + end) / 2));
-  const root = new Node(arr[mid]);
-
-  root.left(buildTree(arr, start, mid - 1));
-  root.right(buildTree(arr, mid + 1, end));
-
-  return root;
-}
-
 export default class Tree {
   constructor(arr) {
-    this.root = null;
+    this.root = this.buildTree(arr);
+  }
+
+  buildTree(arr, start = 0, end = arr.length - 1) {
+    if (start > end) {
+      return null;
+    }
+
+    let mid = Math.floor(parseInt((start + end) / 2));
+    const root = new Node(arr[mid]);
+
+    root.left = this.buildTree(arr, start, mid - 1);
+    root.right = this.buildTree(arr, mid + 1, end);
+
+    return root;
   }
 }
 
@@ -38,5 +38,4 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 
 const BST = new Tree(testArr);
-BST.buildTree(testArr, 0, 13);
-console.log(BST);
+prettyPrint(BST.root);
